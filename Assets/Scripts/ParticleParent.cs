4af900b5f6a1b0f76c9fc8dea2fc0fa16d4badb0
Jie Yang <yajie@microsoft.com>
2018-11-14 17:30:32 +0800

    add particles

---
+++ Assets/Scripts/ParticleParent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleParent : MonoBehaviour {
+    private GameObject ball;
+    private List<Transform> particleList = new List<Transform>();
+    private int activeIdx = 0;
+
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.Find("/PingPongBall");
+        foreach (Transform child in transform)
+            particleList.Add(child);
+
+        StartCoroutine(ChangePartical());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ball != null)
+            transform.position = ball.transform.position;
+	}
+
+    private IEnumerator ChangePartical()
+    {
+        while (true)
+        {
+            for (int i = 0; i < particleList.Count; i++)
+                if (i == activeIdx)
+                    particleList[i].gameObject.SetActive(true);
+                else
+                    particleList[i].gameObject.SetActive(false);
+            yield return new WaitForSeconds(8*60);
+            activeIdx = (activeIdx+1) % particleList.Count;
+        }
+    }
+}
